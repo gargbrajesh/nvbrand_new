90beeedd4d4d8369b5c70c25660806dd55cd4718
unknown <gargbrajesh468@gmail.com>
2022-11-11 19:29:26 +0530

    change npm module

--- src/views/LandingPages/IndexView.js
+++ src/views/LandingPages/IndexView.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import { Banner, Topbar, Footer } from './components';
-
-
 const useStyles = makeStyles(theme => ({
   root: {
     height: '70vh !important',
